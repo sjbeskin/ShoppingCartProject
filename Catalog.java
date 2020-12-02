1fad89b03e5f09c3ec747cc6ee660f2d26ac53de
Stefan Beskin <48729031+sjbeskin@users.noreply.github.com>
2020-12-02 13:02:29 -0800

    Added comments

--- Catalog.java
+++ Catalog.java
@@ -4,22 +4,27 @@ public class Catalog{
   private String name;
   private ArrayList<Item> myCatalog = new ArrayList<Item>();
   
+  //constructor
   public Catalog(String name){
     this.name = name;
   }
   
-  public void add(Item toAdd){
+  //add Item to catalog
+  public void add(Item toAdd){ 
     myCatalog.add(toAdd);
   }
   
+  //return the index of a given item
   public Item get(int index){
     return myCatalog.get(index);
   }
   
-  public int size(){
+  //return catalog size
+  public int size(){ 
     return myCatalog.size();
   }
   
+  //return catalog name
   public String getName(){
     return name;
   }
