c9ef5a497049c5e56c910f4cc15d9c102835f87e
Stefan Beskin <48729031+sjbeskin@users.noreply.github.com>
2020-12-02 13:13:00 -0800

    Added comments

--- Item.java
+++ Item.java
@@ -6,19 +6,25 @@ public class Item {
     double price;
     int bulkQuantity; 
     double bulkPrice; 
+    
+    //constructor with name and price parameters
+    //bulkPrice and bulkQuantity are automatically set to equal -1
+    //if the price is negative, throws an IllegalArgumentException 
 
     public Item(String name, double price) {
         if (price < 0) {
             throw new IllegalArgumentException();
         }
-
         
         this.name = name;
         this.price = price; 
         this.bulkPrice = -1;
         this.bulkQuantity = -1; 
     }
-
+    
+    //constructor with name, price, bulkQuantity, and bulkPrice parameters
+    //if the price, bulkQuantity, or bulkPrice is negative, throws an IllegalArgumentException
+    
     public Item(String name, double price, int bulkQuantity, double bulkPrice) {
         if (price < 0 || bulkQuantity < 0 || bulkPrice < 0) {
             throw new IllegalArgumentException();
@@ -30,6 +36,11 @@ public class Item {
         this.bulkPrice = bulkPrice; 
     }
 
+    
+    //calculates the price given a certain quantity of the Item
+    //calculates differently based on whether bulkQuantity or bulkPrice are negative
+    //this is to account for if the first constructor with just name and price parameters was used
+    
     public double priceFor(int quantity) {
         if (quantity < 0) {
             throw new IllegalArgumentException();
@@ -44,7 +55,9 @@ public class Item {
         }
 
     }
-
+    
+    //toString method that includes the name, the formatted price and, if the second constructor was used, bulkPrice and bulkQuantity
+    
     public String toString() {
         NumberFormat nf = NumberFormat.getCurrencyInstance(); 
 
