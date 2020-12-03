b8d7ebdb2b1abc2890aee81d1115fc9604eaf8e7
Stefan Beskin <48729031+sjbeskin@users.noreply.github.com>
2020-12-03 09:36:48 -0800

    Added Comments

--- ItemOrder.java
+++ ItemOrder.java
@@ -4,13 +4,20 @@
 public class ItemOrder {
     private Item myItem;
     private int quantity;
+    
+    
+    //constructor with parameters for a given Item and quantity
     public ItemOrder(Item myItem, int quantity) {
         this.myItem = myItem;
         this.quantity = quantity;
     }
+    
+    //returns the price for the given Item
     public double getPrice() {
         return myItem.priceFor(quantity);
     }
+    
+    //returns the given Item
     public Item getItem() {
         return myItem;
     }
