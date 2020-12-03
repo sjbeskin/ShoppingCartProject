7253b611ce6eb1a461a612543a18b2a4d907c979
Stefan Beskin <48729031+sjbeskin@users.noreply.github.com>
2020-12-03 09:40:46 -0800

    Added Comments

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -5,9 +5,14 @@ import java.util.*;
 public class ShoppingCart {
     private ArrayList<ItemOrder> itemOrders;
     private boolean discount;
+    
+    //constructor that creates a new arraylist of ItemOrders
     public ShoppingCart(){
         itemOrders = new ArrayList<ItemOrder>();
     }
+    
+    //add a new ItemOrder to the shopping cart
+    //if the attempted ItemOrder's Item is already in the cart, replaces the current ItemOrder with the new one
     public void add(ItemOrder newItemOrder) {
         for (int i = 0; i < itemOrders.size(); i++) {
             Item item = itemOrders.get(i).getItem();
@@ -18,9 +23,14 @@ public class ShoppingCart {
         }
         itemOrders.add(newItemOrder);
     }
+    
+    //set the current discount
     public void setDiscount(boolean discount) {
         this.discount = discount;
     }
+    
+    //returns the total price of the items
+    //reduces total by 10% if the discount applies
     public double getTotal() {
         double total = 0;
         for (ItemOrder itemOrder : itemOrders) {
